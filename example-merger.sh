9e8b3113dcc51fd38db546ed8e53883c167bb0cf
Roland Shoemaker <rolandshoemaker@gmail.com>
2015-11-12 23:48:44 -0800

    Add an actual example merge script

--- example-merger.sh
+++ example-merger.sh
@@ -2,3 +2,20 @@
 
 # DO SOMETHING MERGE LIKE
 
+# assumes git config has already been setup for the user executing
+# this script (name, email, signing key)
+local_user="rolandshoemaker"
+merge_flags="--no-ff -S"
+
+git fetch origin
+git checkout master
+
+if ["$MERGE_USER" = "$local_user"]; then
+    git merge $merge_flags $MERGE_BRANCH
+else
+    git checkout -b $MERGE_USER-$MERGE_BRANCH master
+    git pull https://github.com/$MERGE_USER/repo.git $MERGE_BRANCH
+    git checkout master
+    git merge $merge_flags $MERGE_USER-$MERGE_BRANCH
+fi
+git push origin master
